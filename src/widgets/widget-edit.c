9bf8c209def8bce3c14f81a8a022add504a897e7
Sergey I. Sharybin <nazgul@school9.perm.ru>
2008-08-17 21:11:18 +0600

    * src/widgets/widget-edit.c: Code formation. Made to work a bit   correctly with Japanese alphabet.

--- src/widgets/widget-edit.c
+++ src/widgets/widget-edit.c
@@ -22,7 +22,7 @@
  * @return zero on success, non-zero on failure
  */
 static int
-edit_destructor                   (w_edit_t *__edit)
+edit_destructor (w_edit_t *__edit)
 {
   if (!__edit)
     {
@@ -42,13 +42,13 @@ edit_destructor                   (w_edit_t *__edit)
  * @return zero on success, non-zero on failure
  */
 static int
-edit_drawer                       (w_edit_t *__edit)
+edit_drawer (w_edit_t *__edit)
 {
-  int i, printed_len, w;
+  int i, printed_len, w, printed_width = 0, dummy;
   size_t len, scrolled, caret_pos;
 
   /* Inherit layout from parent */
-  scr_window_t layout=WIDGET_LAYOUT (__edit);
+  scr_window_t layout = WIDGET_LAYOUT (__edit);
 
   /* Widget is invisible or there is no layout */
   if (!WIDGET_VISIBLE (__edit) || !layout)
@@ -62,27 +62,40 @@ edit_drawer                       (w_edit_t *__edit)
   scr_wnd_font (layout, *__edit->font);
 
   /* Calculate length of text which has to be printed */
-  scrolled  =__edit->scrolled;
-  len       = wcslen (__edit->text.data);
-  w         = __edit->position.width;
+  scrolled = __edit->scrolled;
+  len = wcslen (__edit->text.data);
+  w = __edit->position.width;
   caret_pos = __edit->caret_pos;
 
-  printed_len=MIN (len-scrolled, w-(caret_pos-scrolled==w?1:0));
+  printed_len = MIN (len - scrolled, w - (caret_pos - scrolled == w ? 1 : 0));
 
   /* Print text */
-  for (i=0; i<printed_len; i++)
+  for (i = 0; i < printed_len; i++)
     {
-      scr_wnd_add_wchar (layout, __edit->text.data[i+scrolled]);
+      dummy = wcwidth (__edit->text.data[i + scrolled]);
+      if (printed_width + dummy > w)
+        {
+          /* Printed text will not fit to width of edit box */
+          break;
+        }
+      scr_wnd_add_wchar (layout, __edit->text.data[i + scrolled]);
+
+      if (i + scrolled < __edit->caret_pos)
+        {
+          caret_pos += dummy - 1;
+        }
+
+      printed_width += dummy;
     }
 
   /* Print spaces to make edit needed width */
-  for (i=printed_len; i<__edit->position.width; i++)
+  for (i = printed_width; i < __edit->position.width; i++)
     {
       scr_wnd_putch (layout, ' ');
     }
 
-  scr_wnd_move_caret (layout, __edit->position.x+caret_pos-scrolled,
-    __edit->position.y);
+  dummy = __edit->position.x + caret_pos - scrolled;
+  scr_wnd_move_caret (layout, dummy, __edit->position.y);
 
   scr_wnd_attr_restore (layout);
 
@@ -96,7 +109,7 @@ edit_drawer                       (w_edit_t *__edit)
  * @return zero if callback hasn't handled callback
  */
 static int
-edit_focused                      (w_edit_t *__edit)
+edit_focused (w_edit_t *__edit)
 {
   _WIDGET_CALL_USER_CALLBACK (__edit, focused, __edit);
 
@@ -113,7 +126,7 @@ edit_focused                      (w_edit_t *__edit)
  * @return zero if callback hasn't handled callback
  */
 static int
-edit_blured                       (w_edit_t *__edit)
+edit_blured (w_edit_t *__edit)
 {
   _WIDGET_CALL_USER_CALLBACK (__edit, blured, __edit);
 
@@ -130,7 +143,7 @@ edit_blured                       (w_edit_t *__edit)
  * @param __edit - edit for which buffer will be synchronized
  */
 static void
-edit_sync_buffer                  (w_edit_t *__edit)
+edit_sync_buffer (w_edit_t *__edit)
 {
   /* There is no widget or no allocated buffer */
   if (!__edit || !__edit->text.allocated)
@@ -138,11 +151,11 @@ edit_sync_buffer                  (w_edit_t *__edit)
       return;
     }
 
-  size_t txt_len=0, alloc_len, new_len;
+  size_t txt_len = 0, alloc_len, new_len;
 
   if (__edit->text.data)
     {
-      txt_len=wcslen (__edit->text.data);
+      txt_len = wcslen (__edit->text.data);
     }
 
   new_len = alloc_len = __edit->text.allocated;
@@ -150,27 +163,28 @@ edit_sync_buffer                  (w_edit_t *__edit)
   /*
    * TODO:  Does we really need this checking?
    */
-  if (txt_len<alloc_len)
+  if (txt_len < alloc_len)
     {
-      if (alloc_len-txt_len<=1)
+      if (alloc_len - txt_len <= 1)
         {
           /* Allocated buffer is near to be fully used */
-          new_len+=BUFFER_REALLOC_DELTA;
-        } else
-      if (alloc_len-txt_len>BUFFER_REALLOC_DELTA)
+          new_len += BUFFER_REALLOC_DELTA;
+        }
+      else
+        if (alloc_len - txt_len > BUFFER_REALLOC_DELTA)
         {
           /* Length of current text is much shorter than allocated */
           /* buffer. It'll be better is we'll truncate buffer to */
           /* save memory */
-          new_len=txt_len+BUFFER_REALLOC_DELTA;
-       }
+          new_len = txt_len + BUFFER_REALLOC_DELTA;
+        }
 
-      if (new_len!=alloc_len)
+      if (new_len != alloc_len)
         {
           /* Realloc buffer */
-          __edit->text.data=realloc (__edit->text.data,
-              new_len*sizeof (wchar_t));
-          __edit->text.allocated=new_len;
+          __edit->text.data = realloc (__edit->text.data,
+                                       new_len * sizeof (wchar_t));
+          __edit->text.allocated = new_len;
         }
     }
 }
@@ -181,29 +195,34 @@ edit_sync_buffer                  (w_edit_t *__edit)
  * @param __edit - edit for which validate scrolling information
  */
 static void
-edit_validate_scrolling           (w_edit_t *__edit)
+edit_validate_scrolling (w_edit_t *__edit)
 {
   if (!__edit)
     {
       return;
     }
 
-  size_t scrolled      = __edit->scrolled;
+  size_t scrolled = __edit->scrolled, i;
 
   /* Relative position of caret */
-  size_t caret_pos_rel = __edit->caret_pos-scrolled+1;
+  size_t caret_pos_rel = __edit->caret_pos - scrolled + 1;
+
+  for (i = scrolled; i < __edit->caret_pos; ++i)
+    {
+      caret_pos_rel += wcwidth (__edit->text.data[i]) - 1;
+    }
 
   /* Cursor is far too left */
-  if (__edit->caret_pos<__edit->scrolled)
+  if (__edit->caret_pos < __edit->scrolled)
     {
-      __edit->scrolled=__edit->caret_pos;
+      __edit->scrolled = __edit->caret_pos;
     }
   else
     {
       /* Cursor is far too right */
-      if (caret_pos_rel>=__edit->position.width)
+      if (caret_pos_rel >= __edit->position.width)
         {
-          __edit->scrolled+=caret_pos_rel-__edit->position.width;
+          __edit->scrolled += caret_pos_rel - __edit->position.width;
         }
     }
 }
@@ -216,38 +235,55 @@ edit_validate_scrolling           (w_edit_t *__edit)
  * @return zero if callback hasn't handled received character
  */
 static int
-edit_keydown                      (w_edit_t *__edit, wint_t __ch)
+edit_keydown (w_edit_t *__edit, wint_t __ch)
 {
   _WIDGET_CALL_USER_CALLBACK (__edit, keydown, __edit, __ch);
 
   switch (__ch)
     {
-    /* Navigation */
+      /* Navigation */
     case KEY_LEFT:
-      if (__edit->caret_pos>0)
+      if (__edit->caret_pos > 0)
         --__edit->caret_pos;
       break;
     case KEY_RIGHT:
-      if (__edit->caret_pos<wcslen (__edit->text.data))
+      if (__edit->caret_pos < wcslen (__edit->text.data))
         ++__edit->caret_pos;
       break;
     case KEY_HOME:
-      __edit->caret_pos=0;
+      /*
+       * NOTE: IMHO using this stupid cycle is much more convenient
+       *       than hard-core patching edit_validate_scrolling().
+       *       If you'll find a grateful solving of this
+       *       problem - you are welcome :)
+       */
+      while (__edit->caret_pos > 0)
+        {
+          __edit->caret_pos--;
+          edit_validate_scrolling (__edit);
+        }
       break;
     case KEY_END:
-      __edit->caret_pos=wcslen (__edit->text.data);
+      while (__edit->caret_pos < wcslen (__edit->text.data))
+        {
+          __edit->caret_pos++;
+          edit_validate_scrolling (__edit);
+        }
       break;
 
     case KEY_DELETE:
     case KEY_BACKSPACE:
       {
         size_t i, n;
-        n=wcslen (__edit->text.data);
+        n = wcslen (__edit->text.data);
 
         /* If pressed key is a `backspace`, we should */
         /* move caret left by one position. */
-        if (__ch==KEY_BACKSPACE)
+        if (__ch == KEY_BACKSPACE)
           {
+            wchar_t c = __edit->text.data[n - 1], nch;
+            int ch_width = wcwidth (c), dummy;
+
             /* Caret is already in zero position. */
             /* No chars to delete */
             if (!__edit->caret_pos)
@@ -255,23 +291,44 @@ edit_keydown                      (w_edit_t *__edit, wint_t __ch)
                 break;
               }
 
-            --__edit->caret_pos;
-          } else
-            /* There are no chars at the right side from cursor */
-            /* so, there are no chars to delete */
-            if (__edit->caret_pos==n)
+            if (__edit->scrolled > 0)
               {
-                break;
+                /* This dark spell is needed to make cursor to */
+                /* at his old position */
+
+                /*
+                 * TODO: There is some troubles in this stuff
+                 */
+                while (__edit->scrolled > 0)
+                  {
+                    nch = __edit->text.data[__edit->scrolled];
+                    dummy = wcwidth (nch);
+                    if (ch_width - dummy < 0)
+                      {
+                        break;
+                      }
+                    ch_width -= dummy;
+                    --__edit->scrolled;
+                  }
               }
+            --__edit->caret_pos;
+          }
+        else
+          /* There are no chars at the right side from cursor */
+          /* so, there are no chars to delete */
+          if (__edit->caret_pos == n)
+          {
+            break;
+          }
 
         /* Shift text */
-        for (i=__edit->caret_pos; i<n-1; i++)
+        for (i = __edit->caret_pos; i < n - 1; i++)
           {
-            __edit->text.data[i]=__edit->text.data[i+1];
+            __edit->text.data[i] = __edit->text.data[i + 1];
           }
 
         /* Set new null-terminator */
-        __edit->text.data[n-1]=0;
+        __edit->text.data[n - 1] = 0;
       }
 
       break;
@@ -294,21 +351,21 @@ edit_keydown                      (w_edit_t *__edit, wint_t __ch)
           /* Character is printable, so we have to handle this */
           /* callback ourselves */
 
-          size_t i, len=wcslen (__edit->text.data);
+          size_t i, len = wcslen (__edit->text.data);
 
           /* Synchronize buffer to make sure that there is */
           /* enough memory to append character */
           edit_sync_buffer (__edit);
 
           /* Shift buffer */
-          for (i=len; i>__edit->caret_pos; --i)
+          for (i = len; i > __edit->caret_pos; --i)
             {
-              __edit->text.data[i]=__edit->text.data[i-1];
+              __edit->text.data[i] = __edit->text.data[i - 1];
             }
 
           /* Insert new character into text */
-          __edit->text.data[__edit->caret_pos]=__ch;
-          __edit->text.data[len+1]=0;
+          __edit->text.data[__edit->caret_pos] = __ch;
+          __edit->text.data[len + 1] = 0;
 
           /* Move caret to new position */
           __edit->caret_pos++;
@@ -339,8 +396,8 @@ edit_keydown                      (w_edit_t *__edit, wint_t __ch)
  * @return pointer to a edit object
  */
 w_edit_t*
-widget_create_edit                (w_container_t *__parent,
-                                   int __x, int __y, int __width)
+widget_create_edit (w_container_t *__parent,
+                    int __x, int __y, int __width)
 {
   w_edit_t *res;
 
@@ -354,9 +411,9 @@ widget_create_edit                (w_container_t *__parent,
                edit_destructor, edit_drawer,
                __x, __y, 1, __width, 1);
 
-  WIDGET_CALLBACK (res, keydown) = (widget_keydown_proc)edit_keydown;
-  WIDGET_CALLBACK (res, focused) = (widget_action)edit_focused;
-  WIDGET_CALLBACK (res, blured)  = (widget_action)edit_blured;
+  WIDGET_CALLBACK (res, keydown) = (widget_keydown_proc) edit_keydown;
+  WIDGET_CALLBACK (res, focused) = (widget_action) edit_focused;
+  WIDGET_CALLBACK (res, blured) = (widget_action) edit_blured;
 
   res->font = &FONT (CID_BLACK, CID_CYAN);
 
@@ -374,27 +431,28 @@ widget_create_edit                (w_container_t *__parent,
  * @param __text - text to be set
  */
 void
-w_edit_set_text                   (w_edit_t* __edit, wchar_t *__text)
+w_edit_set_text (w_edit_t* __edit, wchar_t *__text)
 {
   if (!__edit || !__text)
     {
       return;
     }
 
-  size_t new_len=wcslen (__text)+1;
+  size_t new_len = wcslen (__text) + 1;
 
-  if (new_len>__edit->text.allocated)
+  if (new_len > __edit->text.allocated)
     {
       /* Length of new string is longer than length of allocated buffer, */
       /* so we need to increase this buffer */
-      __edit->text.data=realloc (__edit->text.data, new_len*sizeof (wchar_t));
-      __edit->text.allocated=new_len;
+      __edit->text.data = realloc (__edit->text.data,
+                                   new_len * sizeof (wchar_t));
+      __edit->text.allocated = new_len;
     }
 
   wcscpy (__edit->text.data, __text);
 
   /* Move caret to end of text */
-  __edit->caret_pos=wcslen (__edit->text.data);
+  __edit->caret_pos = wcslen (__edit->text.data);
 
   edit_validate_scrolling (__edit);
 
@@ -407,7 +465,7 @@ w_edit_set_text                   (w_edit_t* __edit, wchar_t *__text)
  * @param __edit - edit box from which get the text
  */
 wchar_t*
-w_edit_get_text                   (w_edit_t* __edit)
+w_edit_get_text (w_edit_t* __edit)
 {
   if (!__edit)
     {
@@ -424,7 +482,7 @@ w_edit_get_text                   (w_edit_t* __edit)
  * @param __font - font of default text in normal state
  */
 void
-w_edit_set_fonts                  (w_edit_t *__edit, scr_font_t *__font)
+w_edit_set_fonts (w_edit_t *__edit, scr_font_t *__font)
 {
   if (!__edit)
     {
