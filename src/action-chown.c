00e4a070caf8deb059411c544e4045d4e9a741bc
Sergey I. Sharybin <g.ulairi@gmail.com>
2008-10-21 23:29:36 +0600

    * src/action-chown-iface.c, src/action-chown-iface.h,   src/action-chown.c: Turned to use action_operate(). Dropped out   unused functions.

--- src/action-chown.c
+++ src/action-chown.c
@@ -58,6 +58,16 @@
       } \
   }
 
+/* Datatype for operator callback */
+typedef struct
+{
+  /* IDs of new user and group */
+  int uid, gid;
+
+  /* Are multiply files/directories selected */
+  BOOL multiselect;
+} op_data_t;
+
 /* Scan directories before chown'ing */
 /* (to display total progress) */
 static BOOL scan = TRUE;
@@ -90,53 +100,23 @@ get_initial_ids (const wchar_t *__path, const file_panel_item_t **__list,
 }
 
 /**
- * Set name of currently chown'ing file of directory
+ * Chown operation for action_operate
  *
- * @param __proc_wnd - process window descriptor
- * @param __file - name of currently chown'ing file or directory
- */
-static void
-set_current_file (chown_process_window_t *__proc_wnd, const wchar_t *__file)
-{
-  int width;
-  wchar_t buf[1024];
-  width = __proc_wnd->window->position.width - 2;
-
-  fit_dirname (__file, MIN (width, BUF_LEN (buf)), buf);
-
-  w_text_set (__proc_wnd->text, buf);
-}
-
-/**
- * Call chown() function for specified entry (file or directory)
- *
- * @param __path - path to file
- * @param __uid - user id
- * @param __gid - group id
- * @param __dir - is entry a directory?
- * @param __multiselect - are a lot of items selected?
- * This parameter used to determine which message box should be used.
- * @param __proc_wnd - process window descriptor
+ * @param __full_name - full name of file to operate on
+ * @param __stat - file's stat information
+ * @param __data - data collected from user to use
  * @return zero on success, non-zero otherwise
  */
 static int
-chown_entry (const wchar_t *__path, int __uid, int __gid,
-             BOOL __dir, BOOL __multiselect,
-             chown_process_window_t *__proc_wnd)
+chown_operation (const wchar_t *__path, vfs_stat_t __stat,
+                 op_data_t *__data, unsigned int __flags ATTR_UNUSED)
 {
   wchar_t *mask;
   int res;
   int (*msg_proc) (const wchar_t*, ...);
 
-  if (__proc_wnd->progress && !__proc_wnd->manual_total_count)
-    {
-      w_progress_set_pos (__proc_wnd->progress,
-                          w_progress_get_pos (__proc_wnd->progress) + 1);
-    }
-
-  set_current_file (__proc_wnd, __path);
-
-  if (__dir)
+  mask = _(L"Cannot change owner of directory \"%ls\":\n%ls");
+  if (S_ISDIR (__stat.st_mode))
     {
       mask = _(L"Cannot change owner of directory \"%ls\":\n%ls");
     }
@@ -145,7 +125,7 @@ chown_entry (const wchar_t *__path, int __uid, int __gid,
       mask = _(L"Cannot change owner of file \"%ls\":\n%ls");
     }
 
-  if (__multiselect)
+  if (__data->multiselect)
     {
       msg_proc = action_error_retryskipcancel_ign;
     }
@@ -154,7 +134,7 @@ chown_entry (const wchar_t *__path, int __uid, int __gid,
       msg_proc = action_error_retrycancel;
     }
 
-  ACTION_REPEAT (res = vfs_chown (__path, __uid, __gid),
+  ACTION_REPEAT (res = vfs_chown (__path, __data->uid, __data->gid),
                  msg_proc,
                  return ACTION_CANCEL_TO_ABORT (__dlg_res_),
                  mask, __path, vfs_get_error (res));
@@ -167,438 +147,6 @@ chown_entry (const wchar_t *__path, int __uid, int __gid,
   return ACTION_OK;
 }
 
-/**
- * Call chown() function for specified file
- *
- * @param __path - path to file
- * @param __uid - user id
- * @param __gid - group id
- * @param __multiselect - are a lot of items selected?
- * This parameter used to determine which message box should be used.
- * @param __proc_wnd - process window descriptor
- * @return zero on success, non-zero otherwise
- */
-static int
-chown_file (const wchar_t *__path, int __uid, int __gid, BOOL __multiselect,
-            chown_process_window_t *__proc_wnd)
-{
-  return chown_entry (__path, __uid, __gid, FALSE, __multiselect, __proc_wnd);
-}
-
-/**
- * Call chown() function for specified directory
- *
- * @param __path - path to file
- * @param __uid - user id
- * @param __gid - group id
- * @param __multiselect - are a lot of items selected?
- * This parameter used to determine which message box should be used.
- * @param __proc_wnd - process window descriptor
- * @return zero on success, non-zero otherwise
- */
-static int
-chown_dir (const wchar_t *__path, int __uid, int __gid, BOOL __multiselect,
-           chown_process_window_t *__proc_wnd)
-{
-  return chown_entry (__path, __uid, __gid, TRUE, __multiselect, __proc_wnd);
-}
-
-/**
- * Call chown() function for specified item
- *
- * @param __path - base path to item
- * @param __item - item for which chown() will be called
- * @param __uid - user id
- * @param __gid - group id
- * @param __multiselect - are a lot of items selected?
- * This parameter used to determine which message box should be used.
- * @para __proc_wnd - process window descriptor
- * @return zero on success, non-zero otherwise
- */
-static int
-do_chown_item (const wchar_t *__path, file_panel_item_t *__item,
-               int __uid, int __gid, BOOL __multiselect,
-               chown_process_window_t *__proc_wnd)
-{
-  int res;
-  wchar_t *full;
-
-  /* Get full file name */
-  full = wcdircatsubdir (__path, __item->file->name);
-
-  res = chown_entry (full, __uid, __gid,
-                     S_ISDIR (__item->file->lstat.st_mode), __multiselect,
-                     __proc_wnd);
-
-  /* Free unused memory */
-  free (full);
-
-  if (res == ACTION_OK)
-    {
-      /* Item is now unselected */
-      __item->selected = FALSE;
-    }
-
-  return res;
-}
-
-/**
- * Chown top-level items only (do not use dfs)
- *
- * @param __path - path to items
- * @param __list - list of items
- * @param __count - count of items in list
- * @param __uid - user id
- * @param __gid - group id
- * @param __proc_wnd - process window descriptor
- * @return count of items which where successfully chown'ed
- */
-static unsigned long
-do_toplevel_chown (const wchar_t *__path, const file_panel_item_t **__list,
-                   unsigned long __count, int __uid, int __gid,
-                   chown_process_window_t *__proc_wnd)
-{
-  unsigned long i, res = 0;
-  int r;
-
-  w_progress_set_max (__proc_wnd->progress, __count);
-
-  for (i = 0; i < __count; ++i)
-    {
-      r = do_chown_item (__path, (file_panel_item_t*)__list[i],
-                         __uid, __gid, __count > 1, __proc_wnd);
-
-      if (r == ACTION_OK)
-        {
-          ++res;
-        }
-
-      /* User aborted chown'ing after  */
-      if (r == ACTION_ABORT)
-        {
-          break;
-        }
-
-      /* Process accumulated queue */
-      hook_call (L"switch-task-hook", NULL);
-
-      if (__proc_wnd->abort)
-        {
-          break;
-        }
-    }
-
-  return res;
-}
-
-/**
- * Recursively directory chown'ing
- *
- * @param __path - full path to item
- * @param __uid - new user id
- * @param __gid - new group id
- * @param __tree - prescanned tree
- * @param __proc_wnd - process window descriptor
- * @return zero on success, non-zero otherwise
- */
-static int
-chown_dir_rec (wchar_t *__path, int __uid, int __gid,
-               const action_listing_tree_t *__tree,
-               chown_process_window_t *__proc_wnd)
-{
-  int i, res, count, global_res, ignored_items = 0;
-  vfs_dirent_t **eps = NULL;
-  BOOL prescanned= FALSE;
-  wchar_t *full_name;
-  size_t len;
-
-  /* Get listing of a directory */
-  if (__tree)
-    {
-      /* Get listing from prescanned data */
-      count = __tree->count;
-      eps = __tree->dirent;
-
-      prescanned = TRUE;
-
-      /*
-       * NOTE: If we use prescanned data, we shouldn't free()
-       *       directory entries from it. They will be freed while
-       *       the whole prescanned tree will be destroying
-       */
-    }
-  else
-    {
-      /* Scan directory */
-      ACTION_REPEAT (count = vfs_scandir (__path, &eps, 0, vfs_alphasort);
-                    res = count < 0 ? count : 0,
-                    action_error_retryskipcancel_ign,
-                    return ACTION_CANCEL_TO_ABORT (__dlg_res_),
-                    _(L"Cannot listing directory \"%ls\":\n%ls"),
-                    __path, vfs_get_error (res));
-    }
-
-  len = wcslen (__path) + MAX_FILENAME_LEN + 1;
-  full_name = malloc ((len + 1) * sizeof (wchar_t));
-
-  /* Chown children */
-  global_res = ACTION_OK;
-  for (i = 0; i < count; ++i)
-    {
-      if (!IS_PSEUDODIR (eps[i]->name))
-        {
-          /* Get full filename of current file or directory */
-          swprintf (full_name, len, L"%ls/%ls", __path, eps[i]->name);
-
-          if (isdir (full_name))
-            {
-              res = chown_dir_rec (full_name, __uid, __gid,
-                                   prescanned ? __tree->items[i] : NULL,
-                                   __proc_wnd);
-            }
-          else
-            {
-              res = chown_file (full_name, __uid, __gid, TRUE, __proc_wnd);
-            }
-        }
-
-      if (res == ACTION_ABORT)
-        {
-          FREE_REMAIN_DIRENT ();
-          global_res = ACTION_ABORT;
-          break;
-        }
-
-      if (res == ACTION_IGNORE || res == ACTION_SKIP)
-        {
-          ++ignored_items;
-          res = 0;
-        }
-
-      if (!prescanned)
-        {
-          vfs_free_dirent (eps[i]);
-        }
-
-      /* Process accamulated queue of characters */
-      hook_call (L"switch-task-hook", NULL);
-
-      if (__proc_wnd->abort)
-        {
-          /* Free allocated memory */
-          FREE_REMAIN_DIRENT ();
-          global_res = ACTION_ABORT;
-          break;
-        }
-    }
-
-  if (!prescanned)
-    {
-      SAFE_FREE (eps);
-    }
-
-  if (global_res == ACTION_OK)
-    {
-      global_res = chown_dir (__path, __uid, __gid, TRUE, __proc_wnd);
-    }
-
-  return global_res;
-}
-
-/**
- * Iterator for recursively chown'ing
- *
- * @param __path - full path to item
- * @param __uid - new user id
- * @param __gid - new group id
- * @param __tree - prescanned tree
- * @param __proc_wnd - process window descriptor
- * @return zero on success, non-zero otherwise
- */
-static int
-do_rec_chown_iter (wchar_t *__path, int __uid, int __gid,
-                   const action_listing_tree_t *__tree,
-                   chown_process_window_t *__proc_wnd)
-{
-  int res = ACTION_OK;
-
-  if (!isdir (__path))
-    {
-      /* Chown single file (regular or symlink) */
-
-      /* Set `multiselect` flag to truth because */
-      /* at least one more directory selected */
-      res = chown_file (__path, __uid, __gid, TRUE, __proc_wnd);
-    }
-  else
-    {
-      res = chown_dir_rec (__path, __uid, __gid, __tree, __proc_wnd);
-    }
-
-  return res;
-}
-
-/**
- * Recursively chown'ing of items
- *
- * @param __path - path to items
- * @param __list - list of items
- * @param __count - count of items in list
- * @param __uid - user id
- * @param __gid - group id
- * @param __proc_wnd - process window descriptor
- * @return count of items which where successfully chown'ed
- */
-static unsigned long
-do_recursively_chown (const wchar_t *__path, const file_panel_item_t **__list,
-                      unsigned long __count, int __uid, int __gid,
-                      chown_process_window_t *__proc_wnd)
-{
-  int res;
-  unsigned long i, j, source_count = __count, count = 0;
-  BOOL scanned = FALSE;
-  action_listing_t listing;
-  wchar_t *name, *full_name;
-  file_panel_item_t *item;
-
-  if (scan)
-    {
-      ACTION_REPEAT (res = action_get_listing (__path, __list,
-                                               __count, &listing, FALSE, TRUE);
-                     if (res == ACTION_ABORT)
-                       {
-                         return 0;
-                       },
-                     action_error_retryskipcancel_ign,
-                     return 0,
-                     _(L"Cannot get listing of items:\n%ls"),
-                     vfs_get_error (res));
-
-      if (!res)
-        {
-          /* User can ignore some subtrees, so we need */
-          /* get count of source elements from prescanned data */
-          source_count = listing.tree->count;
-          scanned = TRUE;
-
-          w_progress_set_max (__proc_wnd->progress, listing.count);
-        }
-      else
-        {
-          __proc_wnd->manual_total_count = TRUE;
-        }
-    }
-
-  if (!scanned)
-    {
-      w_progress_set_max (__proc_wnd->progress, source_count);
-      __proc_wnd->manual_total_count = TRUE;
-    }
-
-  item = NULL;
-  j = 0;
-  for (i = 0; i < source_count; ++i)
-    {
-      if (scanned)
-        {
-          name = listing.tree->dirent[i]->name;
-
-          /* Search file panel item with specified name */
-          item = (file_panel_item_t*)__list[j++];
-          while ((wcscmp (item->file->name, name) != 0) && j < __count)
-            {
-              item = (file_panel_item_t*)__list[j++];
-            }
-        }
-      else
-        {
-          item = (file_panel_item_t*)__list[j++];
-          name = item->file->name;
-        }
-
-      /* Get full name of item to be deleted */
-      full_name = wcdircatsubdir (__path, name);
-      res = do_rec_chown_iter (full_name, __uid, __gid,
-                               scanned ? listing.tree->items[i] : NULL,
-                               __proc_wnd);
-      free (full_name);
-
-      if (__proc_wnd->manual_total_count && __proc_wnd->progress)
-        {
-          w_progress_set_pos (__proc_wnd->progress,
-                              w_progress_get_pos (__proc_wnd->progress) + 1);
-        }
-
-      if (res == ACTION_OK)
-        {
-          if (item)
-            {
-              item->selected = FALSE;
-              ++count;
-            }
-        }
-      else
-        {
-          if (res == ACTION_ABORT)
-            {
-              break;
-            }
-        }
-
-      /* Process accumulated queue */
-      hook_call (L"switch-task-hook", NULL);
-
-      if (__proc_wnd->abort)
-        {
-          break;
-        }
-    }
-
-  if (scanned)
-    {
-      action_free_listing (&listing);
-    }
-
-  return count;
-}
-
-/**
- * Call chown() function for all items from list
- *
- * @param __path - path to items
- * @param __list - list of items
- * @param __count - count of items in list
- * @param __uid - user id
- * @param __gid - group id
- * @param __rec - do recursively chown'ing
- * @return count of items which where successfully chown'ed
- */
-static unsigned long
-do_chown (const wchar_t *__path, const file_panel_item_t **__list,
-          unsigned long __count, int __uid, int __gid, BOOL __rec)
-{
-  unsigned long res;
-  chown_process_window_t *proc_wnd;
-
-  /* Create and show process window */
-  proc_wnd = action_chown_create_proc_wnd (!__rec || scan);
-  w_window_show (proc_wnd->window);
-
-  if (!__rec)
-    {
-      res = do_toplevel_chown (__path, __list, __count,
-                               __uid, __gid, proc_wnd);
-    }
-  else
-    {
-      res = do_recursively_chown (__path, __list, __count,
-                                  __uid, __gid, proc_wnd);
-    }
-
-  action_chown_destroy_proc_wnd (proc_wnd);
-
-  return res;
-}
 
 /**
  * Determine is it able to make recursively chown'ing
@@ -643,10 +191,10 @@ action_chown (file_panel_t *__panel)
 {
   unsigned long count;
   file_panel_item_t **list = NULL;
-  int uid, gid;
   unsigned long res;
   wchar_t *cwd;
   BOOL recursively = FALSE;
+  op_data_t op_data = {0, 0, FALSE};
 
   /* Get list of items to be chowned */
   count = file_panel_get_selected_items (__panel, &list);
@@ -665,7 +213,7 @@ action_chown (file_panel_t *__panel)
   cwd = file_panel_get_full_cwd (__panel);
 
   res = get_initial_ids (cwd, (const file_panel_item_t**)list,
-                         count, &uid, &gid);
+                         count, &op_data.uid, &op_data.gid);
 
   /*
    * TODO: What should we do if there is wrong symbolic ling in selection?
@@ -677,18 +225,17 @@ action_chown (file_panel_t *__panel)
                                          count);
 
       /* Show dialog to get new values of uid and git */
-      if (action_chown_dialog (&uid, &gid, &recursively) == ACTION_OK)
+      if (action_chown_dialog (&op_data.uid, &op_data.gid,
+                               &recursively) == ACTION_OK)
         {
-          res = do_chown (cwd, (const file_panel_item_t**)list,
-                          count, uid, gid, recursively);
-
-          /*
-           * TODO: We'd better rewrite this stupid hack
-           */
-          if (__panel->items.selected_count)
-            {
-              __panel->items.selected_count -= res;
-            }
+          op_data.multiselect = count > 1;
+          res = action_operate (_(L"Change owner"),
+                                _(L"Changing owner of file or directory:"),
+                                __panel, cwd, (const file_panel_item_t**)list,
+                                count, recursively, scan,
+                                (action_operator_t)chown_operation,
+                                NULL, (action_operator_t)chown_operation,
+                                &op_data);
         }
     }
 
@@ -704,5 +251,5 @@ action_chown (file_panel_t *__panel)
    * TODO: Maybe we should return ACTION_ERR if res is zero
    */
 
-  return ACTION_OK;
+  return res;
 }
